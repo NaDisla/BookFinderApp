ff10a14f813652ead8075b8ffacafbdbbc5ff121
Nadia Disla <tejadanadiam@gmail.com>
2021-09-29 11:31:53 -0400

    Customize Entry.

---
+++ BookFinderApp/BookFinderApp.Android/Renderers/CustomEntryRenderer.cs
@@ -0,0 +1,31 @@
+﻿using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using BookFinderApp.Droid.Renderers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.Android;
+
+[assembly: ExportRenderer(typeof(Entry), typeof(CustomEntryRenderer))]
+namespace BookFinderApp.Droid.Renderers
+{
+    public class CustomEntryRenderer : EntryRenderer
+    {
+        public CustomEntryRenderer(Context context) : base(context)
+        {
+            
+        }
+        protected override void OnElementChanged(ElementChangedEventArgs<Entry> e)
+        {
+            base.OnElementChanged(e);
+            Control.SetBackgroundColor(Android.Graphics.Color.Transparent);
+            //if (Control != null) { Control.SetBackgroundColor(Android.Graphics.Color.Transparent); }
+        }
+    }
+}
